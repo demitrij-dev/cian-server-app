1f0a15cae551ca0f46a13cce11278c976dcafcee
demitrij-dev <mr.dima0816@gmail.com>
2023-12-12 19:46:03 +0700

    v1.0.0

--- server/index.js
+++ server/index.js
@@ -6,13 +6,17 @@ const AuthRouter = require("./src/Auth/AuthRouter")
 const DataRouter = require("./src/Data/DataRouter")
 const mongoose = require("mongoose")
 
-const PORT = process.env.PORT
-const URL = process.env.URL
+const PORT = 3000
+const URL = "mongodb+srv://root:root@ciandb.slhlhib.mongodb.net/"
 
 const app = express()
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://www.postman.com/');
+    res.header('Access-Control-Allow-Origin', 'https://web.postman.co/');
+    res.header('Access-Control-Allow-Origin', 'https://cian-client-app.vercel.app/');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
